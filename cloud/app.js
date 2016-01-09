c35da9015d274f8d7f2d98741524df75c38b34b0
awe_shi <jingwei_shi@126.com>
2016-01-10 00:44:08 +0800

    valid url with appid

--- cloud/app.js
+++ cloud/app.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override')
+var methodOverride = require('method-override');
+//var appconfig = require('cloud/config/application.js');
+
 var AV = require('leanengine');
 var APP_ID =  'gc7kzpln5airnv3c8wt4bof1kwxyifojctfy5yz28luykpji'; // 你的 app id
 var APP_KEY = '5yau1c8h4q4es3t5ibrxmz9tpjdllf1gekdrosz1blwb6kyn'; // 你的 app key
 var MASTER_KEY = 'bi85xoq39g92v7y4mjuelm66hgmn6opjb50enwfknj39co5e'; // 你的 master key
-
 AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
+
+var token = require("cloud/weixin_interface/token.js");
 
 var xml2js = require('xml2js');
 var weixin = require('cloud/weixin.js');
@@ -63,13 +66,15 @@ app.use(AV.Cloud.CookieSession({ secret: 'scret', maxAge: 3600000, fetchUser: tr
 
 // 强制使用 https
 app.enable('trust proxy');
-app.use(AV.Cloud.HttpsRedirect());
+//app.use(AV.Cloud.HttpsRedirect());
 
 //app.use(methodOverride('_method'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//获得办公室IP地址
 app.get('/remoteip',function(req,res){
+  /*
   console.log(req.headers.x-real-ip);
   var remoteaddress = new AV.Object('RemoteAddress');
   var ips = req.ips;
@@ -77,9 +82,13 @@ app.get('/remoteip',function(req,res){
 
   remoteaddress.set('IPAddress',ip);
   remoteaddress.save();
+  */
   res.send('Success');
 
 });
+
+//刷新Token
+//app.get('/refresh',token.refreshTimer);
 
 app.get('/login', function(req, res) {
   // 渲染登录页面
@@ -90,7 +99,7 @@ app.post('/login', function(req, res) {
   AV.User.logIn(req.body.username, req.body.password).then(function(user) {
     //登录成功，AV.Cloud.CookieSession 会自动将登录用户信息存储到 cookie
     //跳转到profile页面。
-    //console.log('signin successfully: %j', user);
+    console.log('signin successfully: %j', user);
     res.redirect('/profile');
   },function(error) {
     //登录失败，跳转到登录页面
@@ -107,24 +116,24 @@ app.get('/logout', function(req, res) {
   res.redirect('/profile');
 });
 
-// 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
-app.get('/hello', function(req, res) {
-  res.render('hello', { message: 'Congrats, you just set up your app!' });
-});
-
+/*
 app.get('/weixin', function(req, res) {
+  console.log('weixin req.originalUrl:',req.originalUrl);
   console.log('weixin req:', req.query);
-  weixin.exec(req.query, function(err, data) {
+  var params = req.query;
+  weixin.exec(params, function(err, data) {
     if (err) {
       return res.send(err.code || 500, err.message);
     }
     return res.send(data);
   });
-})
+});
 
 app.post('/weixin', function(req, res) {
+  console.log('weixin req.originalUrl:',req.originalUrl);
   console.log('weixin req:', req.body);
-  weixin.exec(req.body, function(err, data) {
+  var params = req.body;
+  weixin.exec(params, function(err, data) {
     if (err) {
       return res.send(err.code || 500, err.message);
     }
@@ -134,7 +143,38 @@ app.post('/weixin', function(req, res) {
     res.set('Content-Type', 'text/xml');
     return res.send(xml);
   });
-})
+});
+*/
+
+app.get('/weixin/:appid', function(req, res) {
+  console.log('weixin req.originalUrl:',req.originalUrl);
+  console.log('weixin req:', req.query);
+  var params = req.query;
+  params.appid = req.params.appid;
+  weixin.exec(params, function(err, data) {
+    if (err) {
+      return res.send(err.code || 500, err.message);
+    }
+    return res.send(data);
+  });
+});
+
+app.post('/weixin/:appid', function(req, res) {
+  console.log('weixin req.originalUrl:',req.originalUrl);
+  console.log('weixin req:', req.body);
+  var params = req.body;
+  params.appid = req.params.appid;
+  weixin.exec(params, function(err, data) {
+    if (err) {
+      return res.send(err.code || 500, err.message);
+    }
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(data);
+    console.log('res:', data)
+    res.set('Content-Type', 'text/xml');
+    return res.send(xml);
+  });
+});
 
 //注册微信管理账号
 app.get('/register',function(req, res) {res.render('weixin/register');});
